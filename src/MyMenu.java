3481e1dfa0acdf7f0a0f5212d55795ed25543ae9
YuriiSolodjuk <57952100+Yura07@users.noreply.github.com>
2020-02-04 14:59:18 +0200

    added db for character

--- src/MyMenu.java
+++ src/MyMenu.java
@@ -14,7 +14,7 @@ public class MyMenu {
         menu();
     }
 
-    Scanner sc = new Scanner(System.in);
+    Scanner input = new Scanner(System.in);
     Level1 level1 = new Level1();
     Level2 level2 = new Level2();
 
@@ -22,47 +22,48 @@ public class MyMenu {
     public void menu() {
         System.out.println("\t =====HELLO! WELCOME TO THE GAME!=====");
         level1.level1(createNewCharacter());
-//        level2.level2(chooseCharacter());
+//        level2.level2(createNewCharacter());
     }
 
-
-//    public Character chooseCharacter(){
-//        Character character = null;
-//        System.out.println("\tEnter your name, please");
-//        String name = sc.nextLine();
-//        boolean isCorrectInput = true;
-//        do {
-//            try {
-//                System.out.println("Please, choose your character: \n 1.Fighter \n 2.Bowman \n 3.Mag");
-//                String choose = sc.nextLine();
-//                switch (choose) {
-//                    case "1":
-//                        character = new Fighter(name, 100, 16);
-//                        break;
-//                    case "2":
-//                        character = new Bowman(name, 100, 13);
-//                        break;
-//                    case "3":
-//                        character = new Mag(name, 100, 20);
-//                        break;
-//                    default:
-//                        throw new IllegalArgumentException();
-//                }
-//                isCorrectInput = false;
-//            } catch (InputMismatchException | NullPointerException | IllegalArgumentException e) {
-//                System.err.println("Incorrect enter. Please try again");
-//            }
-//        } while (isCorrectInput);
-//        return character;
-//    }
 
     public Character createNewCharacter(){
         DataBaseHandler dbHandler = new DataBaseHandler();
-        System.out.println("Enter nick name, choose hero...");
-        Scanner input = new Scanner(System.in);
-
-        Character character = new Fighter(input.nextLine(),"Fighter", input.nextInt(), input.nextBoolean(),input.nextInt());
+        Character character = null;
+        boolean isCorrectInput = true;
+        do {
+            try {
+                System.out.println("Please, choose your character: \n 1.Fighter \n 2.Bowman \n 3.Mag");
+                String choose = input.nextLine();
+                System.out.println("Enter nick name, choose hero...");
+                switch (choose) {
+                    case "1":
+                        character = new Fighter(input.nextLine(),"Fighter", input.nextInt(), input.nextBoolean(),input.nextInt());
+                        break;
+                    case "2":
+                        character = new Bowman(input.nextLine(),"Bowman", input.nextInt(), input.nextBoolean(),input.nextInt());
+                        break;
+                    case "3":
+                        character = new Mag(input.nextLine(),"Mag", input.nextInt(), input.nextBoolean(),input.nextInt());
+                        break;
+                    default:
+                        throw new IllegalArgumentException();
+                }
+                isCorrectInput = false;
+            } catch (InputMismatchException | NullPointerException | IllegalArgumentException e) {
+                System.err.println("Incorrect enter. Please try again");
+            }
+        } while (isCorrectInput);
         dbHandler.createCharacter(character);
         return character;
     }
+
+//    public Character createNewCharacter(){
+//        DataBaseHandler dbHandler = new DataBaseHandler();
+//        System.out.println("Enter nick name, choose hero...");
+//        Scanner input = new Scanner(System.in);
+//
+//        Character character = new Fighter(input.nextLine(),"Fighter", input.nextInt(), input.nextBoolean(),input.nextInt());
+//        dbHandler.createCharacter(character);
+//        return character;
+//    }
 }
